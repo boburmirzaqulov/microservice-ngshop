d768a73b3e8da40a8bdbb3e2235b54bdfe0e6672
Temur98 <ttoshmurodov98@gmail.com>
2023-07-17 14:12:39 +0500

    init

---
+++ discount/src/main/java/io/ngshop/discount/controller/DiscountController.java
@@ -0,0 +1,46 @@
+package io.ngshop.discount.controller;
+
+import io.ngshop.discount.dto.DiscountDto;
+import io.ngshop.discount.service.DiscountService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/discounts")
+@Slf4j
+@RequiredArgsConstructor
+public class DiscountController {
+
+    private final DiscountService discountService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DiscountDto> getDiscountById(@PathVariable Long id){
+        return discountService.getDiscountById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DiscountDto>> getAllDiscount(@RequestParam Optional<Integer> offset, @RequestParam Optional<Integer> limit){
+        return discountService.getAllDiscount(offset,limit);
+    }
+
+    @PostMapping
+    public ResponseEntity<DiscountDto> saveDiscount(@RequestBody DiscountDto discountDto){
+        return discountService.saveDiscount(discountDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<DiscountDto> updateDiscount(@RequestBody DiscountDto discountDto){
+        return discountService.updateDiscount(discountDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDiscount(@PathVariable Long id){
+        discountService.deleteDiscount(id);
+    }
+
+}
