a0e2e64c6a825124b3e6665aba706eb9a89f104e
umarenjiner <umarnormamatov.06@gmail.com>
2023-07-17 06:10:27 -0700

    init

--- discount/src/main/java/io/ngshop/discount/DiscountApplication.java
+++ discount/src/main/java/io/ngshop/discount/DiscountApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscountApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscountApplication.class, args);
 	}
-
 }
