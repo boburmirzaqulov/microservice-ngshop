b0b04b216a77bcab394b85b592ce4f0c1c73af99
user <zarinadesignpardalar@gmail.com>
2023-07-18 23:46:43 +0500

    basket

--- basket/src/main/java/io/ngshop/basket/controller/BasketController.java
+++ basket/src/main/java/io/ngshop/basket/controller/BasketController.java
@@ -1,14 +1,35 @@
 package io.ngshop.basket.controller;
 
+import io.ngshop.basket.Entity.BasketDTO;
+import io.ngshop.basket.service.BasketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("Basket")
 @RequiredArgsConstructor
 public class BasketController {
+    private final BasketService basketService;
+    @GetMapping("/GetBasket/{userName}")
+    public ResponseEntity<BasketDTO> getBasket(@PathVariable String userName){
+        return basketService.getBasket(userName);
+    }
+    @PutMapping("/CreateBasket")
+    public ResponseEntity<BasketDTO> setBasket(@RequestBody BasketDTO basketDTO){
+        return basketService.setBasket(basketDTO);
+    }
+    @PostMapping("/CheckoutV2")
+    public ResponseEntity<BasketDTO> checkoutBasket(@RequestBody BasketDTO basketDTO){
+        return basketService.checkoutBasket(basketDTO);
+    }
+    @DeleteMapping("/DeleteBasket/{userName}")
+    public void deleteBasket(@PathVariable String userName ){
+        basketService.deleteBasket(userName);
+    }
+    @PostMapping()
+    public void createBasket(@RequestBody BasketDTO basketDTO){
+        basketService.createBasket(basketDTO);
+    }
 
 }
--- basket/src/main/java/io/ngshop/basket/service/BasketService.java
+++ basket/src/main/java/io/ngshop/basket/service/BasketService.java
@@ -1,5 +1,17 @@
 package io.ngshop.basket.service;
 
+import io.ngshop.basket.Entity.BasketDTO;
+import org.springframework.http.ResponseEntity;
+
 public interface BasketService {
 
+    ResponseEntity<BasketDTO> getBasket(String userName);
+
+    ResponseEntity<BasketDTO> setBasket(BasketDTO basketDTO);
+
+    ResponseEntity<BasketDTO> checkoutBasket(BasketDTO basketDTO);
+
+    void deleteBasket(String userName);
+
+    void createBasket(BasketDTO basketDTO);
 }
