7b329a1bccca95362316b490673d126ed72e82b3
Ibratjonibrohimov <ibratjonibroximov@gmail.com>
2023-07-19 13:34:26 +0500

    commit

--- discount/src/main/java/io/ngshop/discount/dto/DiscountProductDto.java
+++
@@ -1,9 +0,0 @@
-package io.ngshop.discount.dto;
-
-import io.ngshop.discount.model.Discount;
-
-public record DiscountProductDto (
-        Long id,
-        Discount discount
-) {
-}
---
+++ discount/src/main/resources/templates/data.sql
@@ -0,0 +1,6 @@
+insert into COUPON(PRODUCT_NAME, DESCRIPTION, AMOUNT) values ('product1','deadline 10.08.2023',20);
+insert into COUPON(PRODUCT_NAME, DESCRIPTION, AMOUNT) values ('product2','deadline 10.08.2023',20);
+insert into COUPON(PRODUCT_NAME, DESCRIPTION, AMOUNT) values ('product3','deadline 10.08.2023',25);
+insert into COUPON(PRODUCT_NAME, DESCRIPTION, AMOUNT) values ('product4','deadline 10.08.2023',12);
+
+commit
