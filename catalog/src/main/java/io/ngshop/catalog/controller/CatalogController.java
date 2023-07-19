472ee59ace2a562785f140a88290472cf05a1310
Ibratjonibrohimov <ibratjonibroximov@gmail.com>
2023-07-19 18:25:27 +0500

    init

--- catalog/src/main/java/io/ngshop/catalog/controller/CatalogController.java
+++ catalog/src/main/java/io/ngshop/catalog/controller/CatalogController.java
@@ -1,14 +1,11 @@
 package io.ngshop.catalog.controller;
 
-import io.ngshop.catalog.dto.ProductDTO;
+import io.ngshop.catalog.dto.ProductDto;
 import io.ngshop.catalog.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/Catalog")
@@ -16,7 +13,7 @@ import java.util.List;
 public class CatalogController {
     private final ProductService productService;
     @GetMapping("/GetProductById/{id}")
-        public ResponseEntity<ProductDTO> getProductById(@PathVariable String id){
+        public ResponseEntity<ProductDto> getProductById(@PathVariable String id){
             return productService.getProductById(new ObjectId(id));
     }
 
