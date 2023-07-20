d43381d85e6f39bb5872859d7d7b0eeb8bed637b
Tuvolov Ulugbek <tuvalovulugbek677@gmail.com>
2023-07-20 13:31:57 +0500

    crud

---
+++ basket/src/main/java/io/ngshop/basket/clients/DiscountClient.java
@@ -0,0 +1,15 @@
+package io.ngshop.basket.clients;
+
+import io.ngshop.basket.dto.DiscountDTO;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@FeignClient(name = "discount-service", url = "http://localhost:8081")
+public interface DiscountClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/Discount/{productName}")
+    DiscountDTO getDiscount(@PathVariable String productName);
+}
--- basket/src/main/java/io/ngshop/basket/service/BasketService.java
+++ basket/src/main/java/io/ngshop/basket/service/BasketService.java
@@ -2,7 +2,6 @@ package io.ngshop.basket.service;
 
 import io.ngshop.basket.dto.BasketV2DTO;
 import io.ngshop.basket.dto.response.BasketResponse;
-import io.ngshop.basket.dto.response.BasketRequest;
 import io.ngshop.basket.model.Basket;
 import org.springframework.http.ResponseEntity;
 
@@ -11,7 +10,9 @@ public interface BasketService {
 
     ResponseEntity<BasketResponse> getBasketByUsername(String username);
 
-    ResponseEntity<BasketRequest> createBasket(BasketRequest basketRequest);
+    ResponseEntity<BasketResponse> createBasket(BasketResponse basketResponse);
 
     ResponseEntity<Basket> checkoutBasket(BasketV2DTO basketV2DTO);
+
+    ResponseEntity<Void> deleteBasket(String username);
 }
