e0da27bc918231dd22d6f06669f818cf8bca4a15
Amirxon1601 <amirkxanturakxadjayev@gmail.com>
2023-07-20 15:01:22 +0500

    OrderMapper

--- catalog/src/main/java/io/ngshop/catalog/service/impl/ProductServiceImpl.java
+++ catalog/src/main/java/io/ngshop/catalog/service/impl/ProductServiceImpl.java
@@ -1,17 +1,15 @@
-package io.ngshop.catalog.service.impl;
+package io.ngshop.catalog.service.Impl;
 
-import io.ngshop.catalog.dto.ProductDTO;
+import io.ngshop.catalog.dto.ProductDto;
 import io.ngshop.catalog.mapper.ProductMapper;
 import io.ngshop.catalog.model.Product;
 import io.ngshop.catalog.repository.ProductRepository;
 import io.ngshop.catalog.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,31 +18,16 @@ public class ProductServiceImpl implements ProductService {
     private final ProductMapper productMapper;
 
     @Override
-    public ResponseEntity<ProductDTO> createProduct(ProductDTO productDto) {
+    public ResponseEntity<ProductDto> create(ProductDto productDto) {
         Product product = productMapper.toEntity(productDto);
         Product save = productRepository.save(product);
         return ResponseEntity.ok(productMapper.toDto(save));
     }
 
     @Override
-    public ResponseEntity<ProductDTO> getProductById(ObjectId id) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+    public ResponseEntity<List<ProductDto>> getProducts() {
         List<Product> all = productRepository.findAll();
-        List<ProductDTO> list = all.stream().map(productMapper::toDto).collect(Collectors.toList());
+        List<ProductDto> list = all.stream().map(productMapper::toDto).toList();
         return ResponseEntity.ok(list);
-    }
-
-    @Override
-    public ResponseEntity<ProductDTO> updateProduct(ProductDTO productDTO) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteProduct(Long id) {
-        return null;
     }
 }
