566284f5067fe26a5166d11c534063c06ff8db4a
GiyosiddinSultonov <giyosiddinsultonov941@gmail.com>
2023-07-20 14:16:27 +0500

    null

--- basket/src/main/java/io/ngshop/basket/repository/BasketRepository.java
+++ basket/src/main/java/io/ngshop/basket/repository/BasketRepository.java
@@ -3,5 +3,5 @@ package io.ngshop.basket.repository;
 import org.bson.types.ObjectId;
 import org.springframework.data.redis.repository.cdi.RedisRepositoryBean;
 
-public interface BasketRepository extends RedisRepositoryBean<"model kutadi", ObjectId> {
-}
+//public interface BasketRepository extends RedisRepositoryBean<"model kutadi", ObjectId> {
+//}
