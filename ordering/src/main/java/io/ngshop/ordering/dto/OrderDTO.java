4f279afba533438b84abafb7d5b5357a1a48bdea
Amirxon1601 <amirkxanturakxadjayev@gmail.com>
2023-07-24 13:44:08 +0500

    OrderDTO and ProductDto validation

--- ordering/src/main/java/io/ngshop/ordering/dto/OrderDTO.java
+++ ordering/src/main/java/io/ngshop/ordering/dto/OrderDTO.java
@@ -1,5 +1,7 @@
 package io.ngshop.ordering.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,20 +13,50 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDTO implements Serializable {
+    @NotNull(message = "Id do not null!")
+    @NotEmpty(message = "Id do not empty!")
     private Long id;
+    @NotNull(message = "username do not null!")
+    @NotEmpty(message = "Id do not empty!")
     private String username;
+    @NotNull(message = "username do not null!")
+    @NotEmpty(message = "username do not empty!")
     private Double totalPrice;
-    private String firstName;
-    private String lastName;
+    @NotNull(message = "firstName do not null!")
+    @NotEmpty(message = "firstName do not empty!")
+    private String firstname;
+    @NotNull(message = "lastname do not null!")
+    @NotEmpty(message = "lastname do not empty!")
+    private String lastname;
+    @NotNull(message = "emailAddress do not null!")
+    @NotEmpty(message = "emailAddress do not empty!")
     private String emailAddress;
+    @NotNull(message = "addressline do not null!")
+    @NotEmpty(message = "addressline do not empty!")
     private String addressLine;
+    @NotNull(message = "country do not null!")
+    @NotEmpty(message = "country do not empty!")
     private String country;
+    @NotNull(message = "state do not null!")
+    @NotEmpty(message = "state do not empty!")
     private String state;
+    @NotNull(message = "zipcode do not null!")
+    @NotEmpty(message = "zipcode do not empty!")
     private String zipCode;
+    @NotNull(message = "cardName do not null!")
+    @NotEmpty(message = "cardName do not empty!")
     private String cardName;
+    @NotNull(message = "cardNumber do not null!")
+    @NotEmpty(message = "cardNumber do not empty!")
     private String cardNumber;
+    @NotNull(message = "expiration do not null!")
+    @NotEmpty(message = "expiration do not empty!")
     private String expiration;
+    @NotNull(message = "cvv do not null!")
+    @NotEmpty(message = "cvv do not empty!")
     private String cvv;
+    @NotNull(message = "items do not null!")
+    @NotEmpty(message = "items do not empty!")
     private List<ProductDTO> items;
 
 }
--- ordering/src/main/java/io/ngshop/ordering/dto/ProductDTO.java
+++ ordering/src/main/java/io/ngshop/ordering/dto/ProductDTO.java
@@ -1,5 +1,7 @@
 package io.ngshop.ordering.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
+    @NotNull(message = "Id do not null!")
+    @NotEmpty(message = "Id do not empty!")
+    private String Id;
+    @NotNull(message = "quantity do not null!")
+    @NotEmpty(message = "quantity do not empty!")
     private Integer quantity;
+    @NotNull(message = "imageFile do not null!")
+    @NotEmpty(message = "imageFile do not empty!")
     private String imageFile;
+    @NotNull(message = "price do not null!")
+    @NotEmpty(message = "price do not empty!")
     private Double price;
-    private String id;
+    @NotNull(message = "productName do not null!")
+    @NotEmpty(message = "productName do not empty!")
     private String productName;
 }
